f688f5b0315c1361e9efe526f8110d1e1af3d96c
Sergey Kabanets <skabanets97@gmail.com>
2024-03-18 20:10:22 +0200

    add cntrlWrapper

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,11 @@
-import contactsService from "../services/contactsServices.js";
+import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+
+  res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 
@@ -9,3 +14,7 @@ export const deleteContact = (req, res) => {};
 export const createContact = (req, res) => {};
 
 export const updateContact = (req, res) => {};
+
+export default {
+  getAllContacts: ctrlWrapper,
+};
