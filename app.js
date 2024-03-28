fa771b2751ad26ceab2f57b2309632927669677c
Sergey Kabanets <skabanets97@gmail.com>
2024-03-28 23:02:43 +0200

    add connection with database

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import mongoose from "mongoose";
 
-import contactsRouter from "./routes/contactsRouter.js";
+import "dotenv/config";
 
-const app = express();
+const { DB_HOST } = process.env;
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connnect"))
+  .catch((error) => console.log(error.message));
 
-app.use("/api/contacts", contactsRouter);
+// import express from "express";
+// import morgan from "morgan";
+// import cors from "cors";
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+// import contactsRouter from "./routes/contactsRouter.js";
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+// const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// app.use(morgan("tiny"));
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/contacts", contactsRouter);
+
+// app.use((_, res) => {
+//   res.status(404).json({ message: "Route not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
